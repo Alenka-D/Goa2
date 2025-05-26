5d59340a95161cfc2c6beba9c57544079ef7dd9c
Alena_Dabravvolskaya <155534813+Alenka-D@users.noreply.github.com>
2025-05-26 13:08:14 +0300

    Update screen_authorization0_model.dart
    добавила комментарии к коду

--- lib/pages/authotification/screen_authorization_0/screen_authorization0_model.dart
+++ lib/pages/authotification/screen_authorization_0/screen_authorization0_model.dart
@@ -1,18 +1,32 @@
+// Импорты утилит для авторизации через Firebase
 import '/auth/firebase_auth/auth_util.dart';
+
+// Импорты стилей и вспомогательных виджетов FlutterFlow
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+
+// Импорт библиотеки для работы с графикой
 import 'dart:ui';
+
+// Импорт виджета, связанного с этим модельным классом
 import 'screen_authorization0_widget.dart' show ScreenAuthorization0Widget;
+
+// Импорты стандартных библиотек Flutter
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
+/// Модель состояния для `ScreenAuthorization0Widget`.
+/// Используется для управления жизненным циклом и хранения состояний экрана.
 class ScreenAuthorization0Model
     extends FlutterFlowModel<ScreenAuthorization0Widget> {
+  
+  /// Метод инициализации модели. Вызывается при создании экрана.
   @override
   void initState(BuildContext context) {}
 
+  /// Метод очистки ресурсов. Вызывается при удалении экрана.
   @override
   void dispose() {}
 }
